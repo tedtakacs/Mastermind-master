f8292ad0b2a3acb08376253909e554ae7b0a150c
Ted Takacs <ted.takacs@mmodal.com>
2020-01-18 13:22:55 -0500

    Add project files.

---
+++ MastermindGame/ConsoleMessageWriter.cs
@@ -0,0 +1,82 @@
+﻿using System;
+
+namespace MastermindGame
+{
+    public class ConsoleMessageWriter
+    {
+        private const string BREAK = "*****************";
+        private int codeLength;
+        private int turns;
+        private int startNum;
+        private int endNum;
+        private char rightChar;
+        private char wrongChar;
+
+        public ConsoleMessageWriter(int codeLength, int turns, int startNum, int endNum, char rightChar, char wrongChar)
+        {
+            this.codeLength = codeLength;
+            this.turns = turns;
+            this.startNum = startNum;
+            this.endNum = endNum;
+            this.rightChar = rightChar;
+            this.wrongChar = wrongChar;
+        }
+        public void WriteStartMessageToConsole()
+        {
+            Console.WriteLine("***MASTERMIND***");
+            Console.WriteLine($"I HAVE GENERATED A CODE OF {codeLength} NUMBERS, FROM {startNum}-{endNum}, EACH NUMBER ONLY USED ONCE. CAN YOU CRACK THE CODE IN {turns} TURNS?");
+            Console.WriteLine("AFTER EACH TURN, I WILL SHOW YOU THE RESULTS OF YOUR GUESS.");
+            Console.WriteLine($"{rightChar} : RIGHT NUMBER IN THE RIGHT SLOT");
+            Console.WriteLine($"{wrongChar} : RIGHT NUMBER IN THE WRONG SLOT");
+            Console.WriteLine("A BLANK SLOT MEANS THE NUMBER IS WRONG");
+        }
+
+        public void WriteTurnStartInstructionsToConsole(int turnNum)
+        {
+            Console.WriteLine(BREAK);
+            Console.WriteLine($"TURN {turnNum}");
+            if (turnNum == turns)
+            {
+                Console.WriteLine("!LAST TURN!");
+            }
+            Console.WriteLine("MAKE YOUR GUESS");
+
+            Console.WriteLine($"TYPE A SEQUENCE OF {codeLength} NUMBERS, NO SPACES. EACH NUMBER CAN BE IN THE RANGE FROM {startNum}-{endNum}.");
+            Console.WriteLine("HIT ENTER WHEN YOU ARE DONE.");
+        }
+
+        public void WriteResult(string result)
+        {
+            Console.WriteLine();
+            Console.WriteLine("RESULT");
+            for(int i = 0; i < codeLength; i++)
+            {
+                Console.Write("|");
+            }
+            Console.WriteLine();
+            Console.WriteLine(result);
+        }
+
+        public void WriteLoseMessageToConsole()
+        {
+            Console.WriteLine(BREAK);
+            Console.WriteLine("YOU HAVE RAN OUT OF TURNS. YOU LOSE.");
+        }
+
+        public void WriteWinMessageToConsole()
+        {
+            Console.WriteLine(BREAK);
+            Console.WriteLine("YOU HAVE CRACKED THE CODE. YOU WIN!");
+        }
+
+        public void WriteNotValidCode()
+        {
+            Console.WriteLine("THAT CODE IS NOT VALID. ENTER A VALID CODE.");
+        }
+
+        public void WriteEndLine()
+        {
+            Console.WriteLine("PRESS ENTER TO QUIT");
+        }
+    }
+}
---
+++ MastermindGame/Main.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MastermindGame
+{
+    class Program
+    {
+        private const int CODE_LENGTH = 4;
+        private const int START_NUM = 1;
+        private const int END_NUM = 6;
+        private const int TURNS = 10;
+        private const char RIGHT_CHAR = '+';
+        private const char WRONG_CHAR = '-';
+        private const char EMPTY = ' ';
+
+        static void Main(string[] args)
+        {
+            ConsoleMessageWriter consoleMessageWriter = new ConsoleMessageWriter(CODE_LENGTH, TURNS, START_NUM, END_NUM, RIGHT_CHAR, WRONG_CHAR);
+            List<int> realCode = MakeStartList();
+            consoleMessageWriter.WriteStartMessageToConsole();
+            bool hasWon = false;
+            int turn = 1;
+            while (!hasWon && turn <= TURNS)
+            {
+                hasWon = TakeTurn(consoleMessageWriter, realCode, turn);
+                turn++;
+            }
+            if (hasWon)
+            {
+                consoleMessageWriter.WriteWinMessageToConsole();
+            }
+            else
+            {
+                consoleMessageWriter.WriteLoseMessageToConsole();
+            }
+            consoleMessageWriter.WriteEndLine();
+            Console.ReadLine();
+        }
+
+        private static bool TakeTurn(ConsoleMessageWriter consoleMessageWriter, List<int> realCode, int turn)
+        {
+            consoleMessageWriter.WriteTurnStartInstructionsToConsole(turn);
+            List<int> guessCode = PromptUntilValidGuess(consoleMessageWriter);
+            consoleMessageWriter.WriteResult(GetCompareString(realCode, guessCode));
+
+            return guessCode.SequenceEqual(realCode);
+        }
+
+        private static List<int> PromptUntilValidGuess(ConsoleMessageWriter consoleMessageWriter)
+        {
+            List<int> inputNums = new List<int>();
+            bool foundValidInput = false;
+            while (!foundValidInput)
+            {
+                inputNums.Clear();
+                string userInput = Console.ReadLine();
+                foreach (char digit in userInput)
+                {
+                    if (char.IsDigit(digit))
+                    {
+                        int theNum = int.Parse(digit.ToString());
+                        if (theNum >= START_NUM && theNum <= END_NUM)
+                        {
+                            inputNums.Add(theNum);
+                        }
+                    }
+                }
+                foundValidInput = inputNums.Count == CODE_LENGTH;
+                if (!foundValidInput)
+                {
+                    consoleMessageWriter.WriteNotValidCode();
+                }
+            }
+            return inputNums;
+        }
+
+        private static List<int> MakeStartList()
+        {
+            List<int> allNums = new List<int>();
+            for (int i = START_NUM; i <= END_NUM; i++)
+            {
+                allNums.Add(i);
+            }
+            Random random = new Random();
+            var shuffledList = allNums.OrderBy(x => random.Next());
+            return shuffledList.Take(CODE_LENGTH).ToList();
+        }
+
+        private static string GetCompareString(List<int> realCode, List<int> guessCode)
+        {
+            StringBuilder compareString = new StringBuilder();
+            for (int i = 0; i < realCode.Count; i++)
+            {
+                if (realCode[i] == guessCode[i])
+                {
+                    compareString.Append(RIGHT_CHAR);
+                }
+                else if (realCode.Contains(guessCode[i]))
+                {
+                    compareString.Append(WRONG_CHAR);
+                }
+                else
+                {
+                    compareString.Append(EMPTY);
+                }
+            }
+            return compareString.ToString();
+        }
+    }
+}
